3774586ad0c0adcab761a88aa1fe3f9edf0f5c4c
madhuragd <78024273+madhuragd@users.noreply.github.com>
2023-04-08 00:53:40 -0400

    Update real_betas_gammas.py

--- real_betas_gammas.py
+++ real_betas_gammas.py
@@ -7,25 +7,25 @@ from _pickle import dump,load
 
 Min = 2 # Min. n for doing above calculations
 Max = 9 # Max. n for doing above calculations
-num_opti = 10 # No. of times to optimize
+num_opti = 100 # No. of times to optimize
 typ = 'real' # Type of calculation
 
 
 if __name__=="__main__":
     for n in range(Min,Max):
         o = optimize1(n,num_opti,typ)
-        if 'max_chsh_test_real1.pi' in os.listdir(): # Stores in filename "max_chsh_eig.pi"
-            with open('max_chsh_test_real1.pi','rb') as f:
+        if 'n3to8_real.pi' in os.listdir(): # Stores in filename "max_chsh_eig.pi"
+            with open('n3to8_real.pi','rb') as f:
                 d = load(f)
                 if n in d and o['fun'] < d[n]['fun'] or n not in d:
                 # Stores in file if max_viol(n) was not optimized previously 
                 # or optimization result is better than previous iteration.
                     d[n] = o
-                    with open('max_chsh_test_real1.pi','wb') as f:
+                    with open('n3to8_real.pi','wb') as f:
                         dump(d,f)
         else:
         # In the first run, generates the file max_chsh_eig.pi with
         # a initial run of optimization.
             d = {n:o}
-            with open('max_chsh_test_real1.pi','wb') as f:
+            with open('n3to8_real.pi','wb') as f:
                 dump(d,f)
