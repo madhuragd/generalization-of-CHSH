db614501469cc3b33a76ca53e8fe1ba116d35fb9
madhuragd <78024273+madhuragd@users.noreply.github.com>
2023-04-08 00:51:51 -0400

    Update arithmetic_betas_gammas.py

--- arithmetic_betas_gammas.py
+++ arithmetic_betas_gammas.py
@@ -14,18 +14,18 @@ typ = 'arithmetic' # Type of calculation
 if __name__=="__main__":
     for n in range(Min,Max):
         o = optimize1(n,num_opti,typ)
-        if 'max_chsh_test.pi' in os.listdir(): # Stores in filename "max_chsh_eig.pi"
-            with open('max_chsh_test.pi','rb') as f:
+        if 'max_chsh_eig.pi' in os.listdir(): # Stores in filename "max_chsh_eig.pi"
+            with open('max_chsh_eig.pi','rb') as f:
                 d = load(f)
                 if n in d and o['fun'] < d[n]['fun'] or n not in d:
                 # Stores in file if max_viol(n) was not optimized previously 
                 # or optimization result is better than previous iteration.
                     d[n] = o
-                    with open('max_chsh_test.pi','wb') as f:
+                    with open('max_chsh_eig.pi','wb') as f:
                         dump(d,f)
         else:
         # In the first run, generates the file max_chsh_eig.pi with
         # a initial run of optimization.
             d = {n:o}
-            with open('max_chsh_test.pi','wb') as f:
+            with open('max_chsh_eig.pi','wb') as f:
                 dump(d,f)
