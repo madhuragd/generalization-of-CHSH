560ae4a552aff7b758afca445fc5bc2698648889
madhuragd <78024273+madhuragd@users.noreply.github.com>
2023-05-09 15:52:46 +0530

    Update TMSV_r.py

--- TMSV_r.py
+++ TMSV_r.py
@@ -1,5 +1,5 @@
 
-## Code to calculate the maximal CHSH bound for n-MZI+TMSV setting.
+## Code to calculate the maximal BCCB bound for n-MZI+TMSV setting as a function of squeezing parameter (r).
 
 import numpy as np
 from scipy.optimize import minimize
@@ -26,26 +26,29 @@ def f3(r,t,x,y):
 
 
 def TMSV_r(n,r,params: callable):
+    # Function for calculating BCCBi LHS for n-MZI+TMSV with r as a free parameter
     def inn_fn(x):
         t,beta,gamma = params(n,x)
         bccb_val = (sum([f3(r,t,beta[i],gamma[i]) for i in range(n)]) +  sum([f3(r,t,beta[i],gamma[(i+1)%n]) for i in range(n)]) - 2*f3(r,t,beta[0],gamma[n-1]))
-        return -np.abs(bccb_val) # Negative of expected value
+        return -np.abs(bccb_val) # Negative of the summed value
     return inn_fn
  
 def params(n,x):
-    beta = np.zeros(n,dtype=np.longdouble)
+    # Function for calculating the displacements or measurement settings
+    beta = np.zeros(n,dtype=np.longdouble) 
     gamma = np.zeros(n,dtype=np.longdouble)
-    t = 0
+    t = 0 # phase factor of squeezing parameter
     beta[1:] = x[:n-1] 
     gamma[1:] = x[n-1:]
     return t,beta,gamma
     
-params.size = lambda n: 2*n-2
+params.size = lambda n: 2*n-2 
 
 def max_viol(n,r):
-    m = 100
+    # Function for optimization of BCCBi LHS value
+    m = 100 # No. of runs for optimization
     t = time()
-    min_res = [minimize(TMSV_r(n,r,params),np.random.rand(params.size(n)).astype(np.longdouble)) for _ in range(m)] # Can change m=50 to 100,200,etc. to inc. no. 
+    min_res = [minimize(TMSV_r(n,r,params),np.random.rand(params.size(n)).astype(np.longdouble)) for _ in range(m)] # Minimization protocol
     min_res = min(min_res, key = itemgetter('fun'))
     # print("time: ",time()-t, "min func: ",min_res['fun'])
     return min_res
