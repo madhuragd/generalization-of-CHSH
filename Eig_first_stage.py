b6e2eabe25f9499b9a0fa4a4dd90f177bc7cf9f8
madhuragd <madhuraghoshdastidar@gmail.com>
2023-05-10 12:13:07 +0530

    Added comments

--- Eig_first_stage.py
+++ Eig_first_stage.py
@@ -12,7 +12,7 @@ num_opti = 10 # No. of times to optimize
 
 def max_viol(n):
     def inner_fn(x):
-        D1,D2 = x[:]  # Delta = beta_{i+1} - beta_i = gamma_{i+1} - gamma_i
+        D1,D2 = x[:]  # D1 = beta_n - beta_{n-1} = gamma_1 - gamma_0; D2 = beta_1 - beta_0 = gamma_n - gamma_{n-1}
         gamma = np.array([0]+[D1+D2*i for i in range(n-1)]) # gamma_1 = 0
         beta = np.array([D2*i for i in range(n-1)]+[D2*(n-2)+D1])  # beta_1 = 0
         return np.amin(np.linalg.eigvalsh(LHS_of_BCCB(beta,gamma)))
